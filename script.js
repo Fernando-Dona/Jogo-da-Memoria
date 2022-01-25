bf2a1dbbec65401aa78fc2bf81f2efe922c17207
fernando ruel <fernando246dona@gmail.com>
2022-01-25 18:47:17 -0300

    Jogo-da-Memória-DIO

---
+++ script.js
@@ -0,0 +1,106 @@
+let order =[];
+let clickedOrder= [];
+let score = 0;
+
+//0 - verde
+//1 - vermelho
+//2 - amarelo
+//3 - azul
+
+const blue = document.querySelector( '.blue');
+const red = document.querySelector('.red');
+const green = document.querySelector('.green');
+const yellow = document.querySelector('.yellow');
+
+let shuffleOrder = () => {
+ let colorOrder = Math.floor(Math.random() *4)
+ order[order.length] = colorOrder;
+ clickedOrder = [];
+
+for(let i in order){
+    let elementColor = createColorElement(order[1]);
+    LightColor(elementColor,Number(i) + 1);
+}
+}
+
+//acende a proxima cor
+let LightColor = (element, number) =>{
+    number = number * 500;
+    setTimeout(() => {
+        element.classLIst.add('selected');
+    }, number - 250);
+    setTimeout(() => {
+    element.classList.remove('selected');
+    });
+}
+
+//checa se os botoes clicados são os mesmos da ordem gerada no jogo
+let checkOrder = () => {
+    for(let i in clickedOrder) {
+        if(clickedOrder[i] i= order[i]) {
+            gameOver();
+            break;
+        }
+    }
+    if(clickedOrder.length == order.length){
+        alert('Pontuação: $(score)\nVocê acertou! Iniciando proximo nivel!');
+        nextLevel();
+    }
+}
+
+// função para o clique do usuario
+let click =(color) =>{
+    clickedOrder[clickedOrder.length]= color;
+    createColorElement(color).classList.add('selected');
+    
+    setTimeout(() => {
+        createColorElement(color).classList.remove('selected');
+        checkOrder();
+    },250);
+
+    checkOrder();
+}
+
+//função que retorna cor
+let createColorElement = (color) => {
+    if(color == 0){
+        return green;
+    } else if(color == 1){
+     return red;
+    } else if (color == 2) {
+        return yellow;
+    } else if (color == 3) {
+        return blue;
+    }
+}
+
+//função para proximo nivel do jogo
+let nextLevel = () => {
+    score++;
+    shuffleOrder();
+}
+
+//função para game over
+let gameOver = () => {
+    alert('Pontuação: $(score)!\nVocê perdeu o jogo!\nClique em OK para iniciar um novo jogo');
+    order = ();
+    clickedOrder = ();
+
+    playGame();
+}
+
+//função de inicio do jogo
+let playGame = () => {
+    alert(' Bem vindo ao Gênises ! Iniciando o jogo');
+    score = 0;
+
+    nextLevel()
+}
+
+//eventos de clique para as cores
+green.onclick = () => click(0);
+red.onclick = () => click(1);
+yellow.onclick = () => click(2);
+blue.onclick = () => click(3);
+//Inicio do jogo
+playGame()
